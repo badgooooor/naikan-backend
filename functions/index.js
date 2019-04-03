cc7ea71171a9d164e25982eaa1379b7f7a9f1eaf
borbier <yuttakhanbaingen@gmail.com>
2019-04-03 23:32:05 +0700

    :sparkles: Initialize functions

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,22 @@
-const functions = require('firebase-functions');
+const functions = require('firebase-functions')
+const admin = require('firebase-admin')
+const firebaseHelper = require('firebase-functions-helper')
+const express = require('express')
+const bodyParser = require('body-parser')
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+admin.initializeApp(functions.config().firebase)
+
+const app = express()
+const main = express()
+
+main.use('/api/v1', app)
+main.use(bodyParser.json())
+main.use(bodyParser.urlencoded({ extended: false }))
+
+app.get('/', (req, res) => {
+  res.status(200).send({
+    message: 'API with firebase functions'
+  })
+})
+
+exports.webApi = functions.https.onRequest(main)
