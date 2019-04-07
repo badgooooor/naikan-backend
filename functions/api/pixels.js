fbb92194b8b3e3fb702e7ccdc39ec0bc004e161c
borbier <yuttakhanbaingen@gmail.com>
2019-04-07 18:42:41 +0700

    :construction: Create pixel routes

--- functions/api/pixels.js
+++ functions/api/pixels.js
@@ -1,12 +1,40 @@
 const express = require('express')
+const admin = require('firebase-admin')
+const firebaseHelper = require('firebase-functions-helper')
 const router = express.Router()
 
-const pixelsCollections = 'Pixels'
+const db = admin.firestore()
+const pixelsCollections = 'pixels'
 
 router.get('/', (req, res) => {
   res.status(200).send({
     message: 'Pixels routes'
   })
+})
+
+router.post('/newTodayPixel', (req, res) => {
+  let today = admin.firestore.Timestamp.now()
+  let todayPixel = {
+    angry: req.body.angry,
+    confuse: req.body.confuse,
+    happy: req.body.happy,
+    passive: req.body.passive,
+    sad: req.body.sad,
+    finalEmotion: req.body.finalEmotion,
+    date: today
+  }
+  firebaseHelper.firestore
+    .createNewDocument(db, pixelsCollections, todayPixel)
+    .then(docRef => res.status(200).send({
+      message: 'Create today pixel!',
+      pixel: todayPixel
+    }))
+    .catch((err) => {
+      console.log(err)
+      res.error(400).send({
+        error: err
+      })
+    })
 })
 
 module.exports = router
